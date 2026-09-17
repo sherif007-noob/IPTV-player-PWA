47f235cff408242597d6ea96adb3978a2fac3994
SHERIF NADER MORTADA ELSAYED <sherif.nader07@gmail.com>
2026-09-17 17:08:16 +0300

    fix: install Xtream detail request guard

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './detailRequestGuard.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
