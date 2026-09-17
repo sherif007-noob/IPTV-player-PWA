9e6f7814dc468b65000a0805a60a5467ededfe38
SHERIF NADER MORTADA ELSAYED <sherif.nader07@gmail.com>
2026-09-17 19:30:32 +0300

    fix: disable StrictMode media effect replay

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './detailRequestGuard.ts';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(<App />);
