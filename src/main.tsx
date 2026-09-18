b8d3470d9589d56183a9b1e158a9837739b76bd3
SHERIF NADER MORTADA ELSAYED <sherif.nader07@gmail.com>
2026-09-18 08:10:24 +0300

    perf: enable persistent catalog cache layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './detailRequestGuard.ts';
+import './catalogPersistence.ts';
 
 createRoot(document.getElementById('root')!).render(<App />);
