5c9075db0dab26321ec61410dacfc2637674438b
SHERIF NADER MORTADA ELSAYED <sherif.nader07@gmail.com>
2026-09-18 06:39:58 +0300

    test: update localhost port to 8080

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/api/xtream/proxy?url=http%3A%2F%2Fmar10.sbs%2Fplayer_api.php%3Fusername%3DNasser0100%26password%3D01008850042%26action%3Dget_vod_streams")
+fetch("http://localhost:8080/api/xtream/proxy?url=http%3A%2F%2Fmar10.sbs%2Fplayer_api.php%3Fusername%3DNasser0100%26password%3D01008850042%26action%3Dget_vod_streams")
   .then(res => res.text())
   .then(text => console.log(text.slice(0, 100)))
   .catch(console.error);
